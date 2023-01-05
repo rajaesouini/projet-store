d8170562991686d729607d30cacf364ecc19c3d8
adamslaurent <adams.khalfi@gmail.com>
2023-01-05 10:43:13 +0100

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Cart from "./assets/components/pages/cart/Cart";
-import Category from "./assets/components/pages/category/Category";
-import Home from "./assets/components/pages/home/Home";
-import Menu from "./assets/components/pages/menu/Menu";
-import Productdetail from "./assets/components/pages/productdetail/Productdetail";
 import { RouterProvider } from "react-router-dom";
-import router from "./assets/services/Router";
+import router from "./services/Router";
 
 
 function App() {
-	const [count, setCount] = useState('');
+
 
   return (
     <>

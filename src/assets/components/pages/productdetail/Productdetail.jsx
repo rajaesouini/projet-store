6f076ef7937ecbf3557b2ffe48575ff8d2f65f74
adamslaurent <adams.khalfi@gmail.com>
2023-01-05 10:09:15 +0100

    test

--- src/assets/components/pages/productdetail/Productdetail.jsx
+++ src/assets/components/pages/productdetail/Productdetail.jsx
@@ -1,23 +1,45 @@
-
+import { useEffect, useState } from "react";
 import Head from "../../head/Head";
 import Product from "../../product/Product";
+import { Link } from "react-router-dom";
+
 
 
 const Productdetail = () => {
 
+  const [productsData, setProductsData ] = useState([]);
 
+  const fetchProducts = async () => {
 
+    const requestInfos = new Request ('https://fakestoreapi.com/products/category/jewelery', {
+      method: 'get'
+    });
+
+    const request = await fetch(requestInfos);
+
+    const response = await request.json();
+
+    return response;
+  }
+
+  useEffect(() => {
+    fetchProducts().then( data => setProductsData(data));
+  })
 
 
 
     return (
-<>
-        <Head />
-        <p>productdetail</p>
-        <Product />
-        </>    )
-};
+      <>
+      <Head />
+        {
+          productsData.map( (value, index) => {
+            return <Product key={index} value={value} />
+          } )
+        }
+      </>
+    )
 
+};
 
 
 
